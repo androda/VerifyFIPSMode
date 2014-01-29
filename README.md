4d94c4d39d01f4a706dd2e05e774ab76f6940390
Jacob Carter <jacob.carter@venafi.com>
2014-01-29 12:58:37 -0700

    Initial source file commit.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 VerifyFIPSMode
 ==============
 
-A simple tool to verify whether your Windows Server 2008 R2 machine is running in FIPS mode
+A simple tool to verify whether your Windows Server 2008 R2 machine is running in FIPS mode.
+
+Double click it, or run from the command line.
+
+If the program spits out a bunch of hashes to your command window, then your machine is not running in FIPS mode - or Microsoft has made changes to its FIPS certification and all the algorithms I have listed as not supported in FIPS mode are now suddenly working.
+
+I created this tool to try and verify some potential instability with Microsoft's FIPS Enabled registry setting:
+http://support.microsoft.com/kb/811833
+
+Windows 2008 and Vista:
+HKLM\System\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy\Enabled
+
+Windows 2003 and XP:
+HKLM\System\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy
---
+++ VerifyFIPSMode/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VerifyFIPSMode {
+	class Program {
+		static void Main(string[] args) {
+			const string test = "The quick brown fox jumped over the lazy dog.";
+
+			//Write out the standard message. Exceptions mean you're in FIPS mode.
+			Console.WriteLine("VerifyFIPSMode Tool");
+			Console.WriteLine("If any unhandled exceptions appear when you run this tool,");
+			Console.WriteLine("this machine is running in FIPS mode.");
+
+#region Microsoft Windows FIPS Compliant:
+
+			HashAlgorithm sha1 = HashAlgorithm.Create("SHA1");
+
+#endregion
+
+#region Not Microsoft Windows FIPS Compliant:
+
+			HashAlgorithm sha256 = HashAlgorithm.Create("SHA256");
+			HashAlgorithm sha384 = HashAlgorithm.Create("SHA384");
+			HashAlgorithm sha512 = HashAlgorithm.Create("sha512");
+			HashAlgorithm ripeMd160 = HashAlgorithm.Create("RIPEMD160");
+			HashAlgorithm md5 = HashAlgorithm.Create("MD5");
+
+#endregion
+			
+			//First, check the FIPS compliant cipher(s):
+			Console.WriteLine("Testing SHA1:");
+			Console.WriteLine(getHashOrErrorString(test, sha1));
+			//Next, the non-FIPS compliant cipher(s)
+			Console.WriteLine("Testing MD5:");
+			Console.WriteLine(getHashOrErrorString(test, md5));
+			Console.WriteLine("Testing RIPEMD160:");
+			Console.WriteLine(getHashOrErrorString(test, ripeMd160));
+			Console.WriteLine("Testing SHA256:");
+			Console.WriteLine(getHashOrErrorString(test, sha256));
+			Console.WriteLine("Testing SHA384:");
+			Console.WriteLine(getHashOrErrorString(test, sha384));
+			Console.WriteLine("Testing SHA512:");
+			Console.WriteLine(getHashOrErrorString(test, sha512));
+			Console.ReadLine();
+		}
+		//
+		//Layout of this method found here:
+		//http://stackoverflow.com/questions/4329909/hashing-passwords-with-md5-or-sha-256-c-sharp
+		//
+		/// <summary>
+		/// Computes the hash of the string passed in.
+		/// </summary>
+		/// <param name="hashMe">The string to hash</param>
+		/// <param name="hasher">The HashAlgorithm to use</param>
+		/// <returns>The hash, or an error with stack trace</returns>
+		static string getHashOrErrorString(string hashMe, HashAlgorithm hasher) {
+			//The destination byte array:
+			byte[] hashed;
+			//Prepare the byte array for hashing:
+			//This system for converting from string to byte[] was found here:
+			//http://stackoverflow.com/questions/472906/net-string-to-byte-array-c-sharp
+			//
+			byte[] testBytes = new byte[hashMe.Length * sizeof(char)];
+			System.Buffer.BlockCopy(hashMe.ToCharArray(), 0, testBytes, 0, testBytes.Length);
+			//Try the hash
+			try {
+				hashed = hasher.ComputeHash(testBytes);
+			} catch (Exception e) {
+				//Return the error
+				return e.Message + "\r\n---\r\n" + e.StackTrace;
+			}
+			//Return the result
+			return BitConverter.ToString(hashed);
+		}
+	}
+}
